d215f1151514d7f7abf9472261829832a7bbe600
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2022-03-26 18:26:57 +0100

    working on obs data

--- src/main.rs
+++ src/main.rs
@@ -80,7 +80,7 @@ fn decompress (fp: &str, mut writer: std::fs::File) -> Result<(), Error> {
     let mut recovered_epoch : String = String::from("");
     // numerical differentiators
     let mut clock_krn = Kernel::new(8);
-    let mut obs_krn : HashMap<Sv, HashMap<String, (Kernel,Kernel,Kernel)>> = HashMap::new();
+    let mut obs_krn : HashMap<Sv, Vec<Kernel>> = HashMap::new();
 
     let mut header_parsed = false;
     let mut new_epoch = true;
@@ -90,7 +90,7 @@ fn decompress (fp: &str, mut writer: std::fs::File) -> Result<(), Error> {
     let mut epoch_size : u16 = 0;
     let mut epoch_count : u16 = 0;
     
-    println!("Decompressing \"{}\"", fp);
+    println!("Decompressing file \"{}\"", fp);
 
     for l in reader.lines() {
         let line = &l.unwrap();
@@ -163,7 +163,7 @@ fn decompress (fp: &str, mut writer: std::fs::File) -> Result<(), Error> {
                 let (_, rem) = &recovered_epoch.split_at(offset);
                 let (e_flag, rem) = rem.split_at(3);
                 let (n, _) = rem.split_at(3);
-                println!("RECOVERED \"{}\"", recovered_epoch.trim());
+                //println!("RECOVERED \"{}\"", recovered_epoch.trim());
                 epoch_flag = u8::from_str_radix(e_flag.trim(), 10)?;
                 epoch_size = u16::from_str_radix(n.trim(), 10)?;
                 new_epoch = false;
@@ -190,13 +190,15 @@ fn decompress (fp: &str, mut writer: std::fs::File) -> Result<(), Error> {
                 };
                 // output recovered epoch
                 // TODO squeeze recovered clk offset
+                let epo = recovered_epoch.as_str();
+                let (epo, systems) = epo.split_at(35);
+                writeln!(writer, "{}", epo)?;
                 match rnx_version.major {
                     1 | 2 => { // Old RINEX
+                        //TODO wrap system#id on as many lines as needed
+                        // relire src/observation.rs pour voir # max par line
                     },
                     _ => { // Modern RINEX
-                        let epo = recovered_epoch.as_str();
-                        let epo = epo.split_at(35).0;
-                        writeln!(writer, "{}", epo)?;
                         if let Some(clk) = clock_offset {
                             writeln!(writer, "         {}", (clk as f64)/1000.0_f64)?
                         }
@@ -204,9 +206,9 @@ fn decompress (fp: &str, mut writer: std::fs::File) -> Result<(), Error> {
                 }
                 is_clock_offset = false
             } else {
+                epoch_count += 1;
                 if epoch_flag > 2 { // epoch event!
                     // maintain content as is
-                    epoch_count += 1;
                     writeln!(writer, "{}", line)?;
                     if epoch_count == epoch_size {
                         epoch_count = 0;
@@ -214,31 +216,73 @@ fn decompress (fp: &str, mut writer: std::fs::File) -> Result<(), Error> {
                     }
                     continue
                 }
-                // write this entry
-                match rnx_version.major {
+                
+                let epo = recovered_epoch.as_str();
+                let offset : usize = std::cmp::min((41 + 3*epoch_count).into(), epo.len());
+                let system = epo.split_at(offset.into()).0;
+                let system = system.split_at(system.len()-3).1; // grab last XXX
+                let sv = Sv::from_str(system)?;
+                let codes = &obs_codes[&sv.constellation];
+                println!("SYSTEM: \"{}\"", system); 
+                // creates kernel for later use
+                if !obs_krn.contains_key(&sv) {
+                    // first init for this system obs code
+                    let mut map : Vec<Kernel> = Vec::with_capacity(12);
+                    for code in codes { // one per OBS code
+                        map.push(Kernel::new(8))
+                    }
+                    obs_krn.insert(sv, map);
+                }
+            
+                let mut obs_count : usize = 0;
+                let mut content = String::new();
+                let mut rem = line.as_str();
+                
+                loop { // retrieves all observation data + flags
+                    if obs_count == codes.len() {
+                        println!("FLAGS! \"{}\"", rem);
+                    }
+                    // from that given epoch line
+                    let next_whitespace = match rem.find(' ') {
+                        Some(offset) => offset+1,
+                        None => break, //EOL
+                    };
+                    let (roi, r) = rem.split_at(next_whitespace);
+                    rem = r;
+                    println!("CODE : \"{}\" - ROI \"{}\"", codes[obs_count], roi);
+                    if roi == " " {
+                        obs_count += 1;
+                        continue
+                    }
+                    let (init_order, data) : (Option<u16>, i64) = match roi.contains('&') {
+                        false => {
+                            (None, i64::from_str_radix(roi.trim(),10)?)
+                        },
+                        true => {
+                            let (init_order, remainder) = roi.split_at(1);
+                            let (_, data) = remainder.split_at(1);
+                            (Some(u16::from_str_radix(init_order,10)?),
+                            i64::from_str_radix(data.trim(),10)?)
+                        },
+                    };
+                    if let Some(order) = init_order {
+                        // (re)init related kernel
+                        /*let mut kernel = &mut obs_krn[&sv][obs_count];
+                        kernel
+                            .init(
+                                order.into(),
+                                Dtype::Numerical(data))
+                                .unwrap()*/
+                    }
+                    obs_count += 1;
+                }
+                // convert this epoch content back to RINEX
+                /*match rnx_version.major {
                     1 | 2 => { // Old RINEX
                     },
                     _ => { // Modern RINEX
-                        let epo = recovered_epoch.as_str();
-                        let offset : usize = std::cmp::min((41 + 3*(epoch_count+1)).into(), epo.len());
-                        let system = epo.split_at(offset.into()).0;
-                        let system = system.split_at(system.len()-3).1; // grab last XXX
-                        let sv = Sv::from_str(system)?;
-                        let codes = &obs_codes[&sv.constellation];
-                        // build a new kernels if need be 
-                        if !obs_krn.contains_key(&sv) {
-                            let mut map : HashMap<String, (Kernel, Kernel, Kernel)> 
-                                = HashMap::with_capacity(16);
-                            for code in codes { // one per OBS code
-                                let krnls = (Kernel::new(8),Kernel::new(0),Kernel::new(0)); // OBS|LLI|SSI
-                                map.insert(code.to_string(), krnls);
-                            }
-                            obs_krn.insert(sv, map);
-                        }
-                        writeln!(writer, "{}", system)?
                     }
-                }
-
+                }*/
                 epoch_count += 1;
                 if epoch_count == epoch_size {
                     epoch_count = 0;
